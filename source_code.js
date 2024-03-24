deb82133206be0cb35d0cbca7247415fd390da6d
Andy Lu (Lu, Yu-An) <86964895+NaoCoding@users.noreply.github.com>
2024-03-25 00:02:04 +0800

    Create source_code.js

---
+++ source_code.js
@@ -0,0 +1,8 @@
+(function() {
+    var STUDENT_ID = "TYPE_YOUR_STUDENTID_HERE"
+    var PASSWORD = "TYPE_YOUR_PASSWORD_HERE"
+    document.querySelector("#username").value = STUDENT_ID
+    document.querySelector("#password").value = PASSWORD
+    document.querySelector("#boxForm > div > form > div.sign-up-btn > button").click()
+
+})();
